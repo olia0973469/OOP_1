a979324caf915b1e15b1599fcf073d96b262638e
olia0973469 <Olha.Tykha.RI.2023@lpnu.ua>
2024-02-28 11:56:34 +0200

    Add project files.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../../oop.1/oop.1/IntRange.h"
+#include "../../oop.1/oop.1/IntRange.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			IntRange n;
+			n.setfirst(2);
+			n.setsecond(7);
+			int f = n.getFirst();
+			int s = n.getSecond();
+			Assert::AreEqual(2, f);
+			Assert::AreEqual(7, s);
+		}
+	};
+}
